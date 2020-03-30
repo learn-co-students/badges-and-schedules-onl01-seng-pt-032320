354d932f9fd51fdaa0d2a6faf80deeb1dd78df0b
Evan Smith <professionalemail@zoho.com>
2020-03-30 20:36:53 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges=[]
+  attendees.each do |badge_name|
+    badges << "Hello, my name is #{badge_name}."
+  end
+  badges
+end
+
+def assign_rooms(attendees)
+  rooms=[]
+  attendees.each do |room_name|
+    rooms << "Hello, #{room_name}! You'll be assigned to room #{attendees.index(room_name)+1}!"
+  end
+  rooms
+end
+
+def printer(attendees)
+  part_one = batch_badge_creator(attendees)
+  part_two = assign_rooms(attendees)
+  
+  part_one.each do |first|
+    puts first
+  end
+  
+  part_two.each do |second|
+  puts second
+end
+end
+
