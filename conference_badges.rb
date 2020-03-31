29401c32d17e0593958f74d30ba62ad88a18aa11
Dina Kendle <dina.kendle@gmail.com>
2020-03-31 19:27:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,10 +11,11 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  speaker_room = Array.new
-  attendees.each_with_index do |attendee,room_assignment[]|
-  speaker_room << "Hello, #{attendee}! You'll be assigned to room #{room_assignment}!"
+  speaker_room = Array.new()
+  attendees.each_with_index do |attendee,index|
+  speaker_room << "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
   speaker_room
 end
+
 # Write your code here.
