53550d1b2793d1cc36e3a17006260073fc95001f
Dina Kendle <dina.kendle@gmail.com>
2020-03-31 18:55:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badge_messages = Array.new()
+  attendees.each do |attendee| 
+  badge_messages << "Hello, my name is #{attendee}."
+  end
+  badge_messages
+end
+
+def assign_rooms(attendees)
+  
+end
 # Write your code here.
