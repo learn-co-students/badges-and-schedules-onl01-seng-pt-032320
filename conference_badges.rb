afc7abe74e6c746bb68c050485c0b463fffc3155
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-01 23:05:30 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,8 +6,6 @@ badge_maker("Arel")
 
 attendee = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-attendee = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
 def batch_badge_creator(attendee)
 badge = []
 attendee.each do |attendees|
@@ -18,10 +16,20 @@ end
 
 batch_badge_creator(attendee)
 
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendee = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def assign_rooms(speakers)
-  speakers.map.with_index(1){|speaker,index| "Hello, #{speaker}! You'll be assigned to room #{index}!"}
+def assign_rooms(attendee)
+  attendee.map.with_index(1){|speaker,index| "Hello, #{speaker}! You'll be assigned to room #{index}!"}
 end
 
-assign_rooms(speakers)
+assign_rooms(attendee)
+
+
+def printer(attendee)
+badges = batch_badge_creator(attendee)
+rooms = assign_rooms(attendee)
+ puts badges
+ puts rooms
+end
+
+printer(attendee)
